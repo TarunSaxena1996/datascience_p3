7b17ac545f50f1e328751fee52d2b0ee5bfbcb11
TarunSaxena1996 <tarunsaxena1000@gmail.com>
2025-06-02 06:02:41 +0530

    pred pipeline, flask application, templates

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -99,7 +99,7 @@ class ModelTrainer:
 
             if best_model_score<0.6:
                 raise CustomException("No best model found")
-            logging.info(f"Best found model on both training and testing dataset")
+            logging.info(f"Best found model on both training and testing dataset {best_model_name}")
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
@@ -109,7 +109,7 @@ class ModelTrainer:
             predicted=best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
-            return r2_square
+            return best_model_name,r2_square
             
 
 
